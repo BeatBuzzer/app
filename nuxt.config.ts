12fdb5ba08afc330620f6c735a588280fb962a03
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-09-26 10:36:16 +0200

    feat(auth): add spotify login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,24 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint']
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxt/eslint',
+        '@sidebase/nuxt-auth'
+    ],
+    auth: {
+        isEnabled: true,
+        disableServerSideAuth: false,
+        originEnvKey: 'AUTH_ORIGIN',
+        baseURL: 'http://localhost:3000/api/auth',
+        provider: {
+            type: 'authjs',
+            trustHost: false
+        },
+        globalAppMiddleware: true,
+        sessionRefresh: {
+            enablePeriodically: 2000,
+            enableOnWindowFocus: true,
+        }
+    }
 })
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  auth: false
+})
+</script>
+
+<template>
+  <div>
+    <h1>Homepage</h1>
+    I am not protected!
+
+    <br>
+    <NuxtLink to="/login">Login</NuxtLink>
+
+    <!--    <SignInCard />-->
+  </div>
+</template>
