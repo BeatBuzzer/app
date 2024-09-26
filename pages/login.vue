856249b6625e402bb375b1e8013c2f782b4df5b5
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-09-26 10:36:16 +0200

    feat(auth): add spotify login

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// Remember to disable the middleware protection from your page!
+definePageMeta({
+  auth: { unauthenticatedOnly: true, navigateAuthenticatedTo: '/' }
+})
+
+const { signIn } = useAuth()
+
+</script>
+
+<template>
+  <div>
+    <p>Sign-In Options:</p>
+    <button @click="signIn">
+      Spotify
+    </button>
+  </div>
+</template>
