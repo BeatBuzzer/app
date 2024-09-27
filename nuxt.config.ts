86f7a2b87841aed3aee0fc1392b1d094c66cc762
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-09-27 15:29:44 +0200

    feat(auth): implement supabase login & add new SplitView Layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,14 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@nuxt/eslint',
-        '@sidebase/nuxt-auth'
-    ],
-    auth: {
-        isEnabled: true,
-        disableServerSideAuth: false,
-        originEnvKey: 'AUTH_ORIGIN',
-        baseURL: 'http://localhost:3000/api/auth',
-        provider: {
-            type: 'authjs',
-            trustHost: false
-        },
-        globalAppMiddleware: true,
-        sessionRefresh: {
-            enablePeriodically: 2000,
-            enableOnWindowFocus: true,
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/supabase'],
+    supabase: {
+        redirectOptions: {
+            login: '/login',
+            callback: '/confirm',
+            include: undefined, // undefined = include all
+            exclude: ['/'], // exclude routes like ['/foo', '/bar/*']
+            cookieRedirect: true,
         }
     }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,49 @@
 <script setup lang="ts">
-definePageMeta({
-  auth: false
+import SplitView from "~/layouts/SplitView.vue";
+
+useSeoMeta({
+  title: "BeatBuzzer",
+  description: "A fun music quiz game"
 })
+
+const supabase = useSupabaseClient()
+const session = useSupabaseSession()
+
+async function signInWithSpotify() {
+  console.log("click")
+  const {error} = await supabase.auth.signInWithOAuth({
+    provider: 'spotify',
+    options: {
+      redirectTo: 'http://localhost:3000/confirm',
+    },
+  });
+  if (error) {
+    console.error('Error:', error.message);
+  }
+}
+
 </script>
 
 <template>
   <div>
-    <h1>Homepage</h1>
-    I am not protected!
 
-    <br>
-    <NuxtLink to="/login">Login</NuxtLink>
+    <SplitView v-if="!session">
+      <template #header/>
+      <template #content/>
 
-    <!--    <SignInCard />-->
+      <template #bottom>
+        <!-- TODO: create component -->
+        <div class="flex justify-center w-3/4 mb-6 border p-3 rounded-full ">
+          <button @click="signInWithSpotify">
+            Sign In with Spotify
+          </button>
+        </div>
+      </template>
+    </SplitView>
+
+    <div v-if="session">
+      Hi, {{ session.user.email }}!
+    </div>
+
   </div>
 </template>
