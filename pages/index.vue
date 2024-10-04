d6aa8f62ea603a3edd8be874f6e2b34525270ebf
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-10-04 13:19:21 +0200

    Revert "enhance(auth): Improve Login Page by introducing components"
    This reverts commit edc9f691dec339f85c72dfd473de18a8aa7b9d34.

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
 import SplitView from "~/layouts/SplitView.vue";
-import LoginProviderStack from "~/component/login/ProviderStack.vue";
 
 useSeoMeta({
   title: "BeatBuzzer",
   description: "A fun music quiz game"
 })
 
-const session = useSupabaseSession();
+const supabase = useSupabaseClient()
+const session = useSupabaseSession()
+
+async function signInWithSpotify() {
+  console.log("click")
+  const {error} = await supabase.auth.signInWithOAuth({
+    provider: 'spotify',
+    options: {
+      redirectTo: 'http://localhost:3000/confirm',
+    },
+  });
+  if (error) {
+    console.error('Error:', error.message);
+  }
+}
 
 </script>
 
@@ -19,7 +32,12 @@ const session = useSupabaseSession();
       <template #content/>
 
       <template #bottom>
-        <LoginProviderStack/>
+        <!-- TODO: create component -->
+        <div class="flex justify-center w-3/4 mb-6 border p-3 rounded-full ">
+          <button @click="signInWithSpotify">
+            Sign In with Spotify
+          </button>
+        </div>
       </template>
     </SplitView>
 
