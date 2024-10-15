3ffc14069ada968e3179416981e392cd34a02e2c
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-10-15 07:47:14 +0200

    feat(homeview): Add HeaderFooterView & populate with default values

---
+++ layouts/HeaderFooterView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col h-screen max-h-screen max-w-full">
+
+    <div class="basis-24 content-center border rounded-3xl">
+      <div class="flex justify-around">
+        <slot name="header"/>
+      </div>
+    </div>
+
+    <div class="flex-1">
+      <slot name="content"/>
+    </div>
+
+    <div class="basis-24 content-center border rounded-3xl">
+      <div class="flex justify-around">
+        <slot name="footer"/>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
 
+import DashboardView from "~/layouts/HeaderFooterView.vue";
+
 useSeoMeta({
   title: "BeatBuzzer",
   description: "A fun music quiz game"
 })
 
-const session = useSupabaseSession()
+//const session = useSupabaseSession()
 </script>
 
 <template>
   <div class="h-screen bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500">
+    <DashboardView>
+      <template #header>
+        <div class="flex basis-1/2">
+          <div class="mr-3">
+            1A
+            <input type="range" name="test" id="">
+          </div>
 
+          <div>
+            2B
+            <input type="range" name="test" id="">
+          </div>
+        </div>
+      </template>
+      <template #footer>
+        <div class="text-4xl">A</div>
+        <div class="text-4xl">B</div>
+        <div class="text-4xl">C</div>
+      </template>
+    </DashboardView>
   </div>
 </template>
