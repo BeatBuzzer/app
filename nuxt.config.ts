9934ae6481d29c2c7a1abc9590cb2e9b7abb527c
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-10-30 12:56:58 +0100

    feat(test): Add frontend tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: false},
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/supabase', '@nuxt/image'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/supabase', '@nuxt/image', '@nuxt/test-utils/module'],
     supabase: {
         redirectOptions: {
             login: '/login',
