9396b44fd111e2e3cf3c6d014116ef6bf086a089
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-10-31 10:20:33 +0100

    chore(docs): add typedoc

--- server/utils/data-validation.ts
+++ server/utils/data-validation.ts
@@ -1,3 +1,13 @@
+/**
+ * Spotify ID regex
+ */
+export const spotifyIDRegex = /^[a-zA-Z0-9]+$/; // base62
+
+/**
+ * Check if a string is a valid Spotify ID
+ * @param spotifyID - The string to check
+ * @returns Whether the string is a valid Spotify ID
+ */
 export function isValidSpotifyID(spotifyID: string): boolean {
-  return spotifyID.match(/^[a-zA-Z0-9]+$/) !== null; // base62
+  return spotifyID.match(spotifyIDRegex) !== null; // base62
 }
