06ef470dc7f54d5b2f87b911353966679646edf1
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-12-02 20:21:48 +0100

    feat(friends): implement GET `/user/friends` endpoint

---
+++ types/api/responses/user.friends.response.ts
@@ -0,0 +1,14 @@
+export interface GetFriendsResponse {
+    friendship_id: bigint,
+    friend_id: string,
+    created_at: string
+    updated_at: string
+}
+
+export interface FriendError {
+    message: string
+}
+
+export interface GetFriendParam {
+    user_id: string
+}
