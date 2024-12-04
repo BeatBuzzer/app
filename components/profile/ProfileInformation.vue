0f81c556db3cef03f8339f56fc8bd5c6445d49e4
synan798 <s.j.kavuncu@gmail.com>
2024-12-04 21:12:41 +0100

    added first draft for profile view

---
+++ components/profile/ProfileInformation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <!-- First inner div -->
+        <div class="flex flex-col items-center justify-center mb-3">
+            <img src="https://i.scdn.co/image/ab6775700000ee855d4c281804e8773208248312"
+                class="w-16 h-16 rounded-full" />
+            <p>Nickname</p>
+        </div>
+        <!-- Second inner div -->
+        <div  class="grid grid-cols-2 gap-4">
+            <div class="bg-gray-200 h-8 rounded-3xl px-3">Streak</div>
+            <div class="bg-gray-200 h-8 rounded-3xl px-3">Games played</div>
+            <div class="bg-gray-200 h-8 rounded-3xl px-3">Placeholder</div>
+            <div class="bg-gray-200 h-8 rounded-3xl px-3">Date joined</div>
+        </div>
+    </div>
+</template>
---
+++ components/profile/ProfileOverview.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Friendlist from './Friendlist.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <div class="p-3">
+            <ProfileInformation class="py-8"/>
+            <Friendlist/>
+        </div>
+    </div>
+</template>
