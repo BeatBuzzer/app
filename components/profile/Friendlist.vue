2ec89e35b9f50093007021e50d41dee77bb9e50a
synan798 <s.j.kavuncu@gmail.com>
2024-12-05 19:29:40 +0100

    removed import, which were added by accident

--- components/profile/Friendlist.vue
+++ components/profile/Friendlist.vue
@@ -1,57 +1,12 @@
-<!--script setup lang="ts">
-import {
-    FriendshipStatus,
-    FriendshipType,
-    type GetFriendsResponse
-} from "@/types/api/user.friends";
-
-const requests: Ref<GetFriendsResponse[]> = useState("incoming_friendships", () => [])
-const friends: Ref<GetFriendsResponse[]> = useState("accepted_friendships", () => [])
-
-const session = useSupabaseSession()
-
-onMounted(async () => {
-    if (session.value) {
-        await getFriendships()
-        friends.value.push(friends.value[0])
-    }
-})
-
-async function getFriendships() {
-    $fetch<GetFriendsResponse[]>('http://localhost:3000/api/v1/user/friends')
-        .then((data) => {
-            requests.value = data.filter((item) => item.request_type == FriendshipType.INCOMING && item.status === FriendshipStatus.PENDING)
-            friends.value = data.filter((item) => item.status === FriendshipStatus.ACCEPTED)
-        });
-}
-
-</script>
-
-<template>
-    <div class="w-full bg-gray-200 p-3 mt-auto rounded-3xl my-3">
-        <p class="my-1 ">Friends</p>
-        <div class="flex space-x-3 overflow-x-auto">
-            <HomeUsersUserBox 
-            v-for="item in friends" :key="item.friend_id" :profile-picture="item.friend_avatar" :name="item.friend_id" :user-turn="false"
-                class="shrink-0 w-[calc(33.33%-1rem)]" />
-        </div>
-    </div>
-</template-->
-
-
 <script setup lang="ts">
 import {
     FriendshipStatus,
     FriendshipType,
     type GetFriendsResponse
 } from "@/types/api/user.friends";
-import { freemem } from "os";
-import { ref } from 'vue';
 
 const requests: Ref<GetFriendsResponse[]> = useState("incoming_friendships", () => [])
 const friends: Ref<GetFriendsResponse[]> = useState("accepted_friendships", () => [])
-const newFriendId = ref('')
-const newFriendAvatar = ref('')
 
 const session = useSupabaseSession()
 
@@ -75,8 +30,6 @@ function addFriend() {
         friend_username: "sfsdf",
     }
     friends.value.push(newFriend)
-    newFriendId.value = ''
-    newFriendAvatar.value = ''
 }
 </script>
 
