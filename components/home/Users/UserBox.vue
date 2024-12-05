53c05a14eb07b0b0c2141bab11ce19f93a942b93
synan798 <s.j.kavuncu@gmail.com>
2024-12-05 21:23:59 +0100

    removed hover effect from play button on user turn

--- components/home/Users/UserBox.vue
+++ components/home/Users/UserBox.vue
@@ -21,7 +21,7 @@
         <NuxtImg :class="['rounded-full w-12 h-12 sm:w-16 sm:h-16 md:w-16 md:h-16 lg:w-16 lg:h-16', props.userTurn ? 'mr-3' : 'mb-2']" :src="props.profilePicture.toString()" :alt="name"/>
         <p class="text-white">{{ props.name }}</p>
         <button v-if="props.userTurn" class="ml-auto p-2 sm:p-3 md:p-4 lg:p-5" @click="console.log(props.name)">
-            <NuxtImg src="icons/playButton.svg" alt="Play" class="w-12 h-12 sm:w-16 sm:h-16 md:w-16 md:h-16 lg:w-16 lg:h-16 hover:bg-black"/>
+            <NuxtImg src="icons/playButton.svg" alt="Play" class="w-12 h-12 sm:w-16 sm:h-16 md:w-16 md:h-16 lg:w-16 lg:h-16"/>
         </button>
     </div>
 </template>
