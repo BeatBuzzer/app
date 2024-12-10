949c17be061e843c399aec7c8ffe4cc2c74c939d
synan798 <s.j.kavuncu@gmail.com>
2024-12-10 09:18:34 +0100

    formatted time string, fits into smaller div when using iPhone SE

--- components/profile/ProfileInformation.vue
+++ components/profile/ProfileInformation.vue
@@ -10,6 +10,12 @@ onMounted(async () => {
 })
 
 const default_avatar = ref('https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg');
+
+const formatDate = (dateString : string) => {
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  const formattedDate = new Intl.DateTimeFormat('en-US', options).format(new Date(dateString));
+  return formattedDate.replace(',', '');
+};
 </script>
 
 <template>
@@ -22,10 +28,10 @@ const default_avatar = ref('https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970
       </div>
 
       <div class="grid grid-cols-2 gap-4">
-        <div class="bg-gray-200 h-8 rounded-3xl px-3">Games played</div>
-        <div class="bg-gray-200 h-8 rounded-3xl px-3">Streak: {{ user?.daily_streak }}</div>
-        <div class="bg-gray-200 h-8 rounded-3xl px-3">Spotify {{user?.spotify_visibility ? 'public' : 'private'}}</div>
-        <div class="bg-gray-200 h-8 rounded-3xl px-3">Joined {{ new Date(session?.user.created_at).toDateString() }}</div>
+        <div class="bg-gray-200 h-8 rounded-3xl px-3 flex items-center justify-center">Games played</div>
+        <div class="bg-gray-200 h-8 rounded-3xl px-3 flex items-center justify-center">Streak: {{ user?.daily_streak }}</div>
+        <div class="bg-gray-200 h-8 rounded-3xl px-3 flex items-center justify-center">Spotify: {{user?.spotify_visibility ? 'public' : 'private'}}</div>
+        <div class="bg-gray-200 h-8 rounded-3xl px-3 flex items-center justify-center">Joined: {{ formatDate(session?.user.created_at) }}</div>
       </div>
     </div>
   </div>
