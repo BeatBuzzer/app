e6ff093c8168e8e7ce8184c5cd411958b452793e
synan798 <s.j.kavuncu@gmail.com>
2024-12-12 12:36:37 +0100

    added modal for sending friend requests

---
+++ components/profile/FriendRequestModal.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { FriendshipAction, FriendshipStatus } from '@/types/api/user.friends';
+import type { GetUserResponse } from '@/types/api/users';
+import { UserViewType } from '@/types/components/users.view';
+
+
+const props = defineProps({
+    name: {
+        type: String,
+        default: 'User'
+    },
+});
+
+const emit = defineEmits(['close-modal', 'refresh']);
+
+const newFriend = ref('')
+const newFriendError = ref('')
+
+const addFriend = async () => {
+  try {
+    await $fetch('/api/v1/user/friends', {
+      method: 'POST',
+      body: JSON.stringify({ receiver_name: newFriend.value }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    emit('refresh');
+    emit('close-modal');
+    newFriendError.value = '';
+    newFriend.value = ''
+  } catch {
+    newFriendError.value = "Please check your input."
+  }
+};
+
+</script>
+
+<template>
+    <div class="modal-overlay">
+        <div class="modal">
+            <!--NuxtImg :src="profilePicture" class="rounded-full h-32 w-32 mb-3" /-->
+            <input v-model="newFriend" class="rounded-3xl pl-2 border border-black">
+            <p v-if="newFriendError" class="error-message">{{ newFriendError }}</p>
+            <button class="bg-yellow-500 hover:bg-yellow-600 text-red-600 my-2" @click="addFriend">Add Friend</button>
+            <button class="bg-indigo-600 hover:bg-indigo-800 my-5 text-white" @click="$emit('close-modal')">Close</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    background-color: #000000da;
+}
+
+.modal {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: white;
+    height: 500px;
+    width: 500px;
+    margin-top: 10%;
+    padding: 60px 0;
+    border-radius: 20px;
+}
+
+.icon-wrapper {
+    aspect-ratio: 1;
+    border-radius: 50%;
+}
+
+button {
+    width: 150px;
+    height: 40px;
+    font-size: 14px;
+    border-radius: 16px;
+}
+</style>
