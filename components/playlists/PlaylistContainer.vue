c24c4114c921cba9c5f19f670e2595bb2053190c
synan798 <s.j.kavuncu@gmail.com>
2024-12-16 21:10:55 +0100

    fixed playlists showing up in every category

--- components/playlists/PlaylistContainer.vue
+++ components/playlists/PlaylistContainer.vue
@@ -12,13 +12,21 @@ const props = defineProps({
     categories: {
         type: Array,
         required: true
+    },
+    playlistIds: {
+        type: Array,
+        required: true
     }
 });
+
+
+const filteredPlaylists = computed(() => 
+    props.playlists.filter((item) => props.playlistIds.includes(item.spotifyId))
+);
 
 </script>
 
 <template>
-
     <div class="w-full bg-gray-200 px-3 pb-1 mt-auto rounded-3xl mb-3 flex flex-col flex-grow-0 overflow-hidden">
         <!-- Fixed Header -->
         <div class="mb-1 text-xs md:text-base mt-2 pt-2">
@@ -28,12 +36,11 @@ const props = defineProps({
         <!-- Scrollable Playlist Boxes -->
         <div class="flex flex-row flex-nowrap overflow-x-auto gap-2">
             <PlaylistsPlaylistBox 
-                v-for="item in playlists" :key="item.id" 
+                v-for="item in filteredPlaylists" 
+                :key="item.id" 
                 :playlist-id="item.spotifyId"
                 :name="item.name" 
                 v-bind="item.cover ? { cover: item.cover } : {}" class="flex-grow-0 flex-shrink-0" />
         </div>
     </div>
-
-
 </template>
