6914cc0d2c6b572bfe2f1387994c6ac400f43b04
synan798 <s.j.kavuncu@gmail.com>
2024-12-22 17:20:22 +0100

    removed getPlaylistItems

--- composables/useSpotify.ts
+++ composables/useSpotify.ts
@@ -95,7 +95,8 @@ export default function useSpotify(playlistId: string) {
             const data = await res.json();
 
             for (const item of data.items) {
-                const itemCount = await getPlaylistItems(item.id);
+                const itemCount = item.tracks.total
+                console.log(itemCount)
                 if (itemCount >= 8) {
                     const exists = userPlaylists.value.some(playlist => playlist.id === item.id);
                     if (!exists) {
@@ -114,29 +115,6 @@ export default function useSpotify(playlistId: string) {
         } catch (error) {
             console.error('Failed to get playlists:', error);
             return null;
-        }
-    }
-
-    async function getPlaylistItems(playlistId: string): Promise<number> {
-        try {
-            const res = await fetch(`https://api.spotify.com/v1/playlists/${ playlistId }/tracks`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${token.value}`,
-                },
-            });
-
-            if (!res.ok) {
-                console.error(`Error: ${res.status} - ${res.statusText}`);
-                return 0;
-            }
-
-            const data = await res.json();
-            
-            return data.items.length;
-        } catch (error) {
-            console.error('Failed to get playlists:', error);
-            return 0;
         }
     }
 
@@ -169,7 +147,6 @@ export default function useSpotify(playlistId: string) {
         followPlaylist,
         unfollowPlaylist,
         getUserPlaylists,
-        getPlaylistItems,
         getTrackCover
     };
 }
