292c5db6473a15cfa013bc0e8cdd767aa660a660
synan798 <s.j.kavuncu@gmail.com>
2024-12-22 14:13:21 +0100

    removed testing files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: {enabled: false},
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/supabase', '@nuxt/image', '@nuxt/icon', '@nuxt/test-utils/module'],
+    modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/supabase', '@nuxt/image', '@nuxt/icon'],
     supabase: {
         redirectOptions: {
             login: '/login',
