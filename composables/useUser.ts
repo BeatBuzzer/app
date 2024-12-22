3c66019636766f1961c0074959a91abfb7af3608
synan798 <s.j.kavuncu@gmail.com>
2024-12-22 01:40:16 +0100

    added totalgames

--- composables/useUser.ts
+++ composables/useUser.ts
@@ -2,6 +2,8 @@ import type {GetUserResponse} from "@/types/api/users";
 
 export const useUser = () => {
     const user = useState<GetUserResponse | null>('user', () => null)
+    const totalGames = useState<number>('userTotalGames', () => 0)
+    const games = useState<string | null>('games', () => null)
     const loading = useState<boolean>('userLoading', () => false)
     const error = useState<string | null>('userError', () => null)
 
@@ -21,11 +23,27 @@ export const useUser = () => {
         }
     }
 
+    const fetchTotalGames = async () => {
+        try {
+            totalGames.value = 0;
+            games.value.past.forEach(item => {
+                if (item.players[0].id === user.value?.id || item.players[1].id === user.value?.id) {
+                    totalGames.value++;
+                }
+            });
+        } catch (err) {
+            error.value = 'An error occurred';
+            console.error('Error fetching user', err)
+        }
+    }
+
     return {
         user,
+        totalGames,
         loading,
         error,
-        fetchUser
+        fetchUser,
+        fetchTotalGames
     }
 }
 
